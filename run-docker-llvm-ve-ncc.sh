081558eaa969d688b3a055bfe15bbd48dc3f40f9
Erich Focht <efocht@gmail.com>
2021-09-24 11:22:46 +0200

    Changed run-docker-* scripts to also run on a normal node, without having a VE.

--- run-docker-llvm-ve-ncc.sh
+++ run-docker-llvm-ve-ncc.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 VENODEID=${VE_NODE_NUMBER:-0}
-VE=$(readlink -f /dev/veslot$VENODEID)
+if [ -e "/dev/veslot$VENODEID" ]; then
+    VE=$(readlink -f /dev/veslot$VENODEID)
+    VEDEVICE_="--device $VE:$VE"
+    VEVEOS_="-v /var/opt/nec/ve/veos:/var/opt/nec/ve/veos:z"
+fi
 docker run -it --rm \
     -v /dev:/dev:z \
-    --device $VE:$VE \
-    -v /var/opt/nec/ve/veos:/var/opt/nec/ve/veos:z \
+    $VEDEVICE_ $VEVEOS_ \
     -v /opt/nec/ve/ncc:/opt/nec/ve/ncc:z \
     -v /opt/nec/ve/nfort:/opt/nec/ve/nfort:z \
     -v /opt/nec/ve/nlc:/opt/nec/ve/nlc:z \
--- run-docker-llvm-ve.sh
+++ run-docker-llvm-ve.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 VENODEID=${VE_NODE_NUMBER:-0}
-VE=$(readlink -f /dev/veslot$VENODEID)
+if [ -e "/dev/veslot$VENODEID" ]; then
+    VE=$(readlink -f /dev/veslot$VENODEID)
+    VEDEVICE_="--device $VE:$VE"
+    VEVEOS_="-v /var/opt/nec/ve/veos:/var/opt/nec/ve/veos:z"
+fi
 docker run -it --rm \
     -v /dev:/dev:z \
-    --device $VE:$VE \
-    -v /var/opt/nec/ve/veos:/var/opt/nec/ve/veos:z \
+    $VEDEVICE_ $VEVEOS_ \
     -v $HOME:$HOME:z \
     -v /etc/passwd:/etc/passwd:ro \
     -v /etc/group:/etc/group:ro \
